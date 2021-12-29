c1d1bcafac4413bfcd16c8930e77d59b6b1bf390
Yasir Kazmi <37070387+yasir-yk@users.noreply.github.com>
2021-12-29 17:22:48 +0500

    bs4 v0.0.1

---
+++ inc/header.php
@@ -0,0 +1,62 @@
+<!-- Bootstrap CSS -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<link rel="icon" type="image/png" sizes="32x32" href="images/favicon-32x32.png">
+<?php if($basename=='index.php'){ ?>
+	<link rel="stylesheet" href="css/home.css">
+<?php } else {?>
+	<link rel="stylesheet" href="css/all.css">
+<?php }?>
+</head>
+<body>
+	<!-- <a class="accessibility" href="#header">Back to top</a>
+		<a class="accessibility" href="#main">Skip to Content</a> -->
+		<div id="wrapper">
+			<header id="header" class="header sticky-top">
+				<div class="topbar py-2 sticky-hide">
+					<div class="container d-flex justify-content-end">
+						<a href="tel:14444444444" class="text-dark mx-2">1-000-000-0000</a>
+						<a href="mailto:info@logo.com" class="text-dark mx-2">info@logo.com</a>
+					</div>
+				</div>
+				<div class="container">
+					<nav class="navbar navbar-expand-lg navbar-light bg-light">
+						<div class="container-fluid">
+							<a class="navbar-brand" href="#">Navbar</a>
+							<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+								<span class="navbar-toggler-icon"></span>
+							</button>
+							<div class="collapse navbar-collapse" id="navbarSupportedContent">
+								<ul class="navbar-nav me-auto mb-2 mb-lg-0" id="nav">
+									<li class="nav-item">
+										<a class="nav-link active" aria-current="page" href="#">Home</a>
+									</li>
+									<li class="nav-item">
+										<a class="nav-link" href="#myTab">Link</a>
+									</li>
+									<li class="nav-item">
+										<a class="nav-link" href="#data1">Link</a>
+									</li>
+									<li class="nav-item">
+										<a class="nav-link" href="#">Dropdown</a>
+										<ul>
+											<li><a href="#">Action</a></li>
+											<li><a href="#">Another action</a></li>
+											<li><a href="#">Something else here</a></li>
+										</ul>
+									</li>
+									<li class="nav-item">
+										<a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+									</li>
+								</ul>
+								<form action="#" class="search-form" method="post">
+									<a href="javascript:void(0)"  class="search-opner"><i class="fa fa-search"></i></a>
+									<div class="field">
+										<input placeholder="search" accesskey="4" class="input" type="search">
+										<button type="submit"><i class="fa fa-search"></i></button>
+									</div>
+								</form>
+							</div>
+						</div>
+					</nav>
+				</div>
+			</header>
---
+++ inner.php
@@ -0,0 +1,57 @@
+<?php $basename = basename($_SERVER['SCRIPT_FILENAME']); ?>
+<!doctype html>
+	<html lang="en">
+	<head>
+		<!-- Required meta tags -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<title>Project Title</title>
+		<?php include("inc/header.php"); ?>
+		<!-- <div class="banner"><img src="https://via.placeholder.com/1920x700.png" class="img-fluid" alt=""></div> -->
+		<main id="main">
+			<nav id="navbar-example2" class="navbar navbar-light bg-light px-3 fixed-top" data-bs-offset="5000">
+				<a class="navbar-brand" href="#">Navbar</a>
+				<ul class="nav nav-pills">
+					<li class="nav-item">
+						<a class="nav-link" href="#scrollspyHeading1">First</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#scrollspyHeading2">Second</a>
+					</li>
+					<li class="nav-item dropdown">
+						<a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Dropdown</a>
+						<ul class="dropdown-menu">
+							<li><a class="dropdown-item" href="#scrollspyHeading3">Third</a></li>
+							<li><a class="dropdown-item" href="#scrollspyHeading4">Fourth</a></li>
+							<li><hr class="dropdown-divider"></li>
+							<li><a class="dropdown-item" href="#scrollspyHeading5">Fifth</a></li>
+						</ul>
+					</li>
+				</ul>
+			</nav>
+			<div data-bs-spy="scroll" data-bs-target="#navbar-example2" data-bs-offset="0" class="scrollspy-example" tabindex="0">
+				<h4 id="scrollspyHeading1">First heading</h4>
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores error odio quis hic quam molestias ex, aperiam quasi molestiae autem tempore iste delectus soluta placeat, tenetur labore praesentium sunt modi.	</p>
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores error odio quis hic quam molestias ex, aperiam quasi molestiae autem tempore iste delectus soluta placeat, tenetur labore praesentium sunt modi.	</p>
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores error odio quis hic quam molestias ex, aperiam quasi molestiae autem tempore iste delectus soluta placeat, tenetur labore praesentium sunt modi.	</p>
+				<h4 id="scrollspyHeading2">Second heading</h4>
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores error odio quis hic quam molestias ex, aperiam quasi molestiae autem tempore iste delectus soluta placeat, tenetur labore praesentium sunt modi.	</p>
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores error odio quis hic quam molestias ex, aperiam quasi molestiae autem tempore iste delectus soluta placeat, tenetur labore praesentium sunt modi.	</p>
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores error odio quis hic quam molestias ex, aperiam quasi molestiae autem tempore iste delectus soluta placeat, tenetur labore praesentium sunt modi.	</p>
+				<h4 id="scrollspyHeading3">Third heading</h4>
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores error odio quis hic quam molestias ex, aperiam quasi molestiae autem tempore iste delectus soluta placeat, tenetur labore praesentium sunt modi.	</p>
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores error odio quis hic quam molestias ex, aperiam quasi molestiae autem tempore iste delectus soluta placeat, tenetur labore praesentium sunt modi.	</p>
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores error odio quis hic quam molestias ex, aperiam quasi molestiae autem tempore iste delectus soluta placeat, tenetur labore praesentium sunt modi.	</p>
+				<h4 id="scrollspyHeading4">Fourth heading</h4>
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores error odio quis hic quam molestias ex, aperiam quasi molestiae autem tempore iste delectus soluta placeat, tenetur labore praesentium sunt modi.	</p>
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores error odio quis hic quam molestias ex, aperiam quasi molestiae autem tempore iste delectus soluta placeat, tenetur labore praesentium sunt modi.	</p>
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores error odio quis hic quam molestias ex, aperiam quasi molestiae autem tempore iste delectus soluta placeat, tenetur labore praesentium sunt modi.	</p>
+				<h4 id="scrollspyHeading5">Fifth heading</h4>
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores error odio quis hic quam molestias ex, aperiam quasi molestiae autem tempore iste delectus soluta placeat, tenetur labore praesentium sunt modi.	</p>
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores error odio quis hic quam molestias ex, aperiam quasi molestiae autem tempore iste delectus soluta placeat, tenetur labore praesentium sunt modi.	</p>
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores error odio quis hic quam molestias ex, aperiam quasi molestiae autem tempore iste delectus soluta placeat, tenetur labore praesentium sunt modi.	</p>
+			</div>
+		</main>
+		<?php include("inc/footer.php"); ?>
